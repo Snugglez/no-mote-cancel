3906b7c350d4bd89d97e48c5650648412b330db0
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-05-21 12:59:30 -0400

    added hook order incase it fucks other skill emulation scripts

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MOTES = [18, 22]
 module.exports = function NoMoteCancel(d) {
-d.hook('C_CANCEL_SKILL', 3, (e) => {
+d.hook('C_CANCEL_SKILL', 3, {order: -99999}, (e) => {
 if(d.game.me.class == 'elementalist' && MOTES.includes(Math.floor(e.skill.id / 10000))){
 return false
 }
