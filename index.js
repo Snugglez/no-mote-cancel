79ff75c37a37a7088efa5c8304b2757a138c533b
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-05-21 12:54:23 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const MOTES = [18, 22]
-
-module.exports = function NoMoteCancel(mod) {
-	let job = -1
-
-	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => { job = event.templateId % 100 - 1 })
-	mod.hook('C_CANCEL_SKILL', 3, event => job === 7 && MOTES.includes(Math.floor(event.skill.id / 10000)) ? false : undefined)
+module.exports = function NoMoteCancel(d) {
+d.hook('C_CANCEL_SKILL', 3, (e) => {
+if(d.game.me.class == 'elementalist' && MOTES.includes(Math.floor(e.skill.id / 10000))){
+return false
+}
+})
 }
